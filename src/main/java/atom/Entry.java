8f92f05d4965db5efbc0be66b583343c56ba39e7
d-smith <doug.smith.mail@gmail.com>
2016-09-07 06:38:42 -0700

    Outlined all methods

---
+++ src/main/java/atom/Entry.java
@@ -0,0 +1,49 @@
+package atom;
+
+public class Entry {
+    private String aggregateId;
+    private int version;
+    private String typeCode;
+    private String published;
+    private String content;
+
+    public String getAggregateId() {
+        return aggregateId;
+    }
+
+    public void setAggregateId(String aggregateId) {
+        this.aggregateId = aggregateId;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
+    public String getTypeCode() {
+        return typeCode;
+    }
+
+    public void setTypeCode(String typeCode) {
+        this.typeCode = typeCode;
+    }
+
+    public String getPublished() {
+        return published;
+    }
+
+    public void setPublished(String published) {
+        this.published = published;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
--- src/main/java/atom/FeedController.java
+++ src/main/java/atom/FeedController.java
@@ -1,5 +1,6 @@
 package atom;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +14,28 @@ public class FeedController {
         feed.setUpdated("today");
 
         return feed;
+    }
+
+    @RequestMapping("/notifications/{feedId}")
+    public Feed feedById(@PathVariable String feedId) {
+        Feed feed = new Feed();
+        feed.setId("1");
+        feed.setTitle("some stuffs");
+        feed.setUpdated("today");
+
+        return feed;
+    }
+
+
+    @RequestMapping("/notifications/{aggregateId}/{version}")
+    public Entry getEntry(@PathVariable String aggregateId, @PathVariable int version) {
+        Entry entry = new Entry();
+        entry.setAggregateId(aggregateId);
+        entry.setContent("xxx==");
+        entry.setPublished("nowish");
+        entry.setTypeCode("ABC");
+        entry.setVersion(version);
+
+        return entry;
     }
 }
